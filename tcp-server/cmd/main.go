2bdd3139064cbb48d7d086ad526e8b42ad6041d9
tomsucho <tomasz.suchorowski@n-able.com>
2023-11-20 16:29:29 +0100

    [Go-examples] first commit

---
+++ tcp-server/cmd/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/textproto"
+)
+
+func handleConnection(conn net.Conn, id int) {
+
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	tp := textproto.NewReader(reader)
+	log.Printf("New connection id:%d from %s \n", id, conn.RemoteAddr())
+
+	for {
+		if text, err := tp.ReadLine(); err != nil {
+			log.Printf("Connection id: %d got %s from %s closed!\n", id, err, conn.RemoteAddr())
+			return
+		} else {
+			log.Print("id: ", id, " Received: ", text)
+		}
+	}
+}
+
+func main() {
+	portNum := flag.Int("port", 8080, "Port number to listen on")
+	flag.Parse()
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", *portNum))
+	if err != nil {
+		log.Fatal(err)
+	}
+	i := 1
+	log.Printf("Listening on %d", *portNum)
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConnection(conn, i)
+		i++
+	}
+}
